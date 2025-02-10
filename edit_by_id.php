d5edc6fd3ef65e079383dce8260b744f4f38a3f0
Your Name <you@example.com>
2025-02-10 14:41:09 +0200

    last

--- edit_by_id.php
+++ edit_by_id.php
@@ -6,20 +6,22 @@ require_once "dbconnect.php";
     $name = $_POST['name'];
     $phone = $_POST['phone'];
     $diagnosis = $_POST['diagnosis'];
-    $query1 = "
-UPDATE `diagnosis`
+    $query1 = "UPDATE `diagnosis`
 SET 
     `diagnosis_name` = '$diagnosis'
 WHERE 
     `patient_id` = $id
-ORDER BY `id`
 LIMIT 1;
-
-
-DELETE FROM `diagnosis`
+UPDATE `patient`
+SET 
+    `name` = '$name', 
+    `phone` = '$phone' 
+WHERE 
+    `id` = $id;
+    DELETE FROM `diagnosis`
 WHERE 
     `patient_id` = $id
-    AND `diagnosis_name` != '$diagnosis' ;";
+    AND `diagnosis_name` != '$diagnosis';";
 
     $pdo->query($query1);
 
